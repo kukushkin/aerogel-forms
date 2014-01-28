9a67c802023ff3e5f1d910951e1c55bee4036f6a
Alex Kukushkin <alex@kukushk.in>
2014-01-28 21:32:28 +0200

    Added input_hidden_tag helper, fixed gemspec to reflect dependency on aerogel-core

--- app/helpers/form_helpers.rb
+++ app/helpers/form_helpers.rb
@@ -4,5 +4,11 @@ module Aerogel::Helpers
     Aerogel::Forms::FormBuilder.new( object, options, &block ).render
   end
 
+  # Renders input hidden tag.
+  #
+  def input_hidden_tag( name, value )
+    tag :input, type: 'hidden', name: name, value: value
+  end
+
 end # module Aerogel::Helpers
 
