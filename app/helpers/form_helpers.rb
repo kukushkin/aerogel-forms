f93a5d72e41d715649a3b729729f64af1ab606b9
Alex Kukushkin <alex@kukushk.in>
2014-02-05 21:27:02 +0200

    Major overhaul: default module context for app/*

--- app/helpers/form_helpers.rb
+++ app/helpers/form_helpers.rb
@@ -1,14 +1,10 @@
-module Aerogel::Helpers
+def form( object = nil, options = {}, &block )
+  Aerogel::Forms::FormBuilder.new( object, options, &block ).render
+end
 
-  def form( object = nil, options = {}, &block )
-    Aerogel::Forms::FormBuilder.new( object, options, &block ).render
-  end
-
-  # Renders input hidden tag.
-  #
-  def input_hidden_tag( name, value )
-    tag :input, type: 'hidden', name: name, value: value
-  end
-
-end # module Aerogel::Helpers
+# Renders input hidden tag.
+#
+def input_hidden_tag( name, value )
+  tag :input, type: 'hidden', name: name, value: value
+end
 
